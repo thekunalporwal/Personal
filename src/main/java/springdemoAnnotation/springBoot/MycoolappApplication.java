e733ccd345568e09ada1b3a9a805768b46691789
kunal3.gupta <kunal3.gupta@paytm.com>
2021-11-23 11:03:35 +0530

    Module add

--- src/main/java/springdemoAnnotation/springBoot/MycoolappApplication.java
+++ src/main/java/springdemoAnnotation/springBoot/MycoolappApplication.java
@@ -2,8 +2,9 @@ package springdemoAnnotation.springBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
