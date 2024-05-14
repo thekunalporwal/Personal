05d62e41b1e6a08e6974ebf42ea6d30380447d78
kunal3.gupta <kunal3.gupta@paytm.com>
2024-05-14 16:43:17 +0530

    logback masked converted fixed

--- src/main/java/logback/Example.java
+++ src/main/java/logback/Example.java
@@ -1,12 +1,14 @@
 package logback;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Slf4j
 public class Example {
-    private static final Logger logger = LoggerFactory.getLogger(Example.class);
 
     public static void main(String[] args) {
-        logger.info("Example log from {}", Example.class.getSimpleName());
+        log.info("Example address deviceId=1235 , address1=kunal log from {}", Example.class.getSimpleName());
+        log.info("Example address log from {}", "hehe");
     }
 }
--- src/main/java/logback/LogMaskingConverter.java
+++ src/main/java/logback/LogMaskingConverter.java
@@ -1,50 +1,37 @@
 package logback;
-/**
- * Created this LogMakingConverter Plug in for masking logs statement
- * Here masking for Credit card number, CVV and SSN .
- * You can also implement for password and rest depend on your application needs.
- */
 
-import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.config.plugins.Plugin;
-import org.apache.logging.log4j.core.pattern.ConverterKeys;
-import org.apache.logging.log4j.core.pattern.LogEventPatternConverter;
+import ch.qos.logback.classic.pattern.MessageConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Plugin(name="LogMasking", category = "Converter")
-@ConverterKeys({"PIImask"})
-public class LogMaskingConverter extends LogEventPatternConverter {
-
-    private static final String ENCRYPT_KEY = "etJS1W+SyXUHXEZgbgK/Pg==";
-    private static final String IV = "012345610abcde";
-
+public class LogMaskingConverter extends MessageConverter {
     /* Regex to identify keys of DTO and Hashmap. */
-    private static final String DTO_REGEX = "(?i)((address1|address2|address3|country|state|city|bankAccountHolderName|bankAccount|firstName|lastName|userId|zipcode|pincode|pincodes|vpa|personalPan|businessPan|panNo|PAN_NO|panNumber|panNoBusiness|GSTIN|serialNo|deviceSerialNo|deviceSerialNumber|PROOF_NO|communicationpin|pin|COMMUNICATION_PIN|accountNo|bankAccNo|BANK_ACCOUNT_NO|authSignatoryIdProofNo)\\s*=\\s*[']?)((?!null\\b)([\\w!.#$%&'*+-/=?^_`{|}~]{1,4})([\\w!.#$%&'*+-/=?^_`{|}~]+))";
+    private static final String DTO_REGEX = "(?i)(deviceId|address1|address2|address3|country|state|city|bankAccountHolderName|bankAccount|firstName|lastName|userId|zipcode|pincode|pincodes|vpa|personalPan|businessPan|panNo|PAN_NO|panNumber|panNoBusiness|GSTIN|serialNo|deviceSerialNo|deviceSerialNumber|PROOF_NO|communicationpin|pin|COMMUNICATION_PIN|accountNo|bankAccNo|BANK_ACCOUNT_NO|authSignatoryIdProofNo)(\\s*=\\s*[']?)(?!null\\b)([\\w!.#$%&'*+-/=?^_`{|}~]{1,4})([\\w!.#$%&'*+-/=?^_`{|}~]+)";
     private static final Pattern DTO_PATTERN = Pattern.compile(DTO_REGEX,Pattern.CASE_INSENSITIVE);
-    private static final String DTO_REPLACEMENT_REGEX = "$1****$5";
-    private static final String PHONE_DTO_REGEX = "(?i)((mobileNumber1|mobileNumber2|alternateMobileNumber|phone|phoneNo|phoneNum|phoneNumber|Phone_Number|Mobile_Number|mobile|mobileNo|mobileNum|mobile_no|mobileNumber)\\s*=\\s*[']?)((?!null\\b)([+][\\w]{2})?((-[\\w]{2})|([\\w]{2}-?))([\\w]{6})([\\w!.#$%&'*+-/=?^_`{|}~]+))";
+    private static final String DTO_REPLACEMENT_REGEX = "$1$2****$4";
+    private static final String PHONE_DTO_REGEX = "(?i)(mobileNumber1|mobileNumber2|alternateMobileNumber|phone|phoneNo|phoneNum|phoneNumber|Phone_Number|Mobile_Number|mobile|mobileNo|mobileNum|mobile_no|mobileNumber)(\\s*=\\s*[']?)(?!null\\b)([+][\\w]{2})?((-[\\w]{2})|([\\w]{2}-?))([\\w]{6})([\\w!.#$%&'*+-/=?^_`{|}~]+)";
     private static final Pattern PHONE_DTO_PATTERN = Pattern.compile(PHONE_DTO_REGEX,Pattern.CASE_INSENSITIVE);
-    private static final String PHONE_DTO_REPLACEMENT_REGEX = "$1$4$5******$9";
+    private static final String PHONE_DTO_REPLACEMENT_REGEX = "$1$2$3$5$6******$8";
 
     /* Regex to identify contact number of JSON objects. */
-    private static final String PHONE_JSON_REGEX = "(\"[\\w\\s]*(mobileNumber1|mobileNumber2|alternateMobileNumber|phone|phoneNo|phoneNum|phoneNumber|Phone_Number|Mobile_Number|mobile|mobileNo|mobileNum|mobile_no|mobileNumber)\"\\s*:\\s*\")(([+][\\w]{2})?((-[\\w]{2})|([\\w]{2}-?))([\\w]{6})([\\w!.#$%&'*+-/=?^_`{|}~]+))";
+    private static final String PHONE_JSON_REGEX = "(\"[\\w\\s]*(mobileNumber1|mobileNumber2|alternateMobileNumber|phone|phoneNo|phoneNum|phoneNumber|Phone_Number|Mobile_Number|mobile|mobileNo|mobileNum|mobile_no|mobileNumber)\"\\s*:\\s*\")([+][\\w]{2})?((-[\\w]{2})|([\\w]{2}-?))([\\w]{6})([\\w!.#$%&'*+-/=?^_`{|}~]+)";
     private static final Pattern PHONE_JSON_PATTERN = Pattern.compile(PHONE_JSON_REGEX,Pattern.CASE_INSENSITIVE);
-    private static final String PHONE_JSON_REPLACEMENT_REGEX = "$1$4$5******$9";
+    private static final String PHONE_JSON_REPLACEMENT_REGEX = "$1$3$5$6******$8";
 
-    private static final String EMAIL_DTO_REGEX = "(?i)((EmailId|email|pEmail|sEmail|contactEmailId|primaryEmail|communicationEmails)\\s*=\\s*[']?)((?!null\\b)([\\w!.#$%&'*+-/=?^_`{|}~]{3})([\\w!.#$%&'*+-/=?^_`{|}~]{3})([\\w!.#$%&'*+-/=?^_`{|}~]+[@])([\\w!.#$%&'*+-/=?^_`{|}~]+))";
+    private static final String EMAIL_DTO_REGEX = "(?i)(EmailId|email|pEmail|sEmail|contactEmailId|primaryEmail|communicationEmails)(\\s*=\\s*[']?)(?!null\\b)([\\w!.#$%&'*+-/=?^_`{|}~]{3})([\\w!.#$%&'*+-/=?^_`{|}~]{3})([\\w!.#$%&'*+-/=?^_`{|}~]+[@])([\\w!.#$%&'*+-/=?^_`{|}~]+)";
     private static final Pattern EMAIL_DTO_PATTERN = Pattern.compile(EMAIL_DTO_REGEX,Pattern.CASE_INSENSITIVE);
-    private static final String EMAIL_DTO_REPLACEMENT_REGEX = "$1***$5******@$7";
+    private static final String EMAIL_DTO_REPLACEMENT_REGEX = "$1$2***$4******@$6";
 
     /* Regex to identify contact number of JSON objects. */
-    private static final String EMAIL_JSON_REGEX = "(\"[\\w\\s]*(EmailId|email|pEmail|sEmail|contactEmailId|primaryEmail|communicationEmails)\"\\s*:\\s*\")(([\\w!.#$%&'*+-/=?^_`{|}~]{3})([\\w!.#$%&'*+-/=?^_`{|}~]{3})([\\w!.#$%&'*+-/=?^_`{|}~]+[@])([\\w!.#$%&'*+-/=?^_`{|}~]+))";
+    private static final String EMAIL_JSON_REGEX = "(\"[\\w\\s]*(EmailId|email|pEmail|sEmail|contactEmailId|primaryEmail|communicationEmails)\"\\s*:\\s*\")([\\w!.#$%&'*+-/=?^_`{|}~]{3})([\\w!.#$%&'*+-/=?^_`{|}~]{3})([\\w!.#$%&'*+-/=?^_`{|}~]+[@])([\\w!.#$%&'*+-/=?^_`{|}~]+)";
     private static final Pattern EMAIL_JSON_PATTERN = Pattern.compile(EMAIL_JSON_REGEX,Pattern.CASE_INSENSITIVE);
-    private static final String EMAIL_JSON_REPLACEMENT_REGEX = "$1***$5******@$7";
+    private static final String EMAIL_JSON_REPLACEMENT_REGEX = "$1***$4******@$6";
 
     /* Regex to identify keys of JSON objects. */
-    private static final String JSON_REGEX = "(\"[\\w\\s]*(address1|address2|address3|country|state|city|bankAccountHolderName|bankAccount|firstName|lastName|userId|zipcode|pincode|pincodes|vpa|personalPan|businessPan|panNo|PAN_NO|panNumber|panNoBusiness|GSTIN|serialNo|deviceSerialNo|deviceSerialNumber|PROOF_NO|communicationpin|pin|COMMUNICATION_PIN|accountNo|bankAccNo|BANK_ACCOUNT_NO|authSignatoryIdProofNo)\"\\s*:\\s*[\"\\[]?)(([\\w!.#$%&'*+-/=?^_`{|}~]{1,4})([\\w!.#$%&'*+-/=?^_`{|}~]+))";
+    private static final String JSON_REGEX = "(\"[\\w\\s]*(deviceId|address1|address2|address3|country|state|city|bankAccountHolderName|bankAccount|firstName|lastName|userId|zipcode|pincode|pincodes|vpa|personalPan|businessPan|panNo|PAN_NO|panNumber|panNoBusiness|GSTIN|serialNo|deviceSerialNo|deviceSerialNumber|PROOF_NO|communicationpin|pin|COMMUNICATION_PIN|accountNo|bankAccNo|BANK_ACCOUNT_NO|authSignatoryIdProofNo)\"\\s*:\\s*[\"\\[]?)([\\w!.#$%&'*+-/=?^_`{|}~]{1,4})([\\w!.#$%&'*+-/=?^_`{|}~]+)";
     private static final Pattern JSON_PATTERN = Pattern.compile(JSON_REGEX,Pattern.CASE_INSENSITIVE);
-    private static final String JSON_REPLACEMENT_REGEX = "$1****$5";
+    private static final String JSON_REPLACEMENT_REGEX = "$1****$4";
 
     /* Regex to identify keys of JSON objects. */
     private static final String URL_DTO_REGEX = "(?i)(URL=)(https?://)?(www\\.)?(?!null\\b)([\\w!.#$%&'*+-/=?^_`{|}~]{1,4})([\\w!.#$%&'*+-/=?^_`{|}~]+)";
@@ -63,71 +50,56 @@ public class LogMaskingConverter extends LogEventPatternConverter {
     private static final String URL_JSON_REGEX = "(\"[\\w\\s]*(URL)\":\\s*\")(https?://)?(www\\.)?([\\w!.#$%&'*+-/=?^_`{|}~]{1,4})([\\w!.#$%&'*+-/=?^_`{|}~]+)";
     private static final Pattern URL_JSON_PATTERN = Pattern.compile(URL_JSON_REGEX,Pattern.CASE_INSENSITIVE);
     private static final String URL_JSON_REPLACEMENT_REGEX = "$1$3$4****$6";
-    public static String encryptionProperty = "false" ;
-
-
-    protected LogMaskingConverter(String name, String style) {
-        super(name, style);
-    }
-
-    public static LogMaskingConverter newInstance(String[] options) {
-        return new LogMaskingConverter("PIImask",Thread.currentThread().getName());
-    }
 
     @Override
-    public void format(LogEvent event, StringBuilder outputMessage) {
-
-        String message = event.getMessage().getFormattedMessage();
-        String maskedMessage = message;
-        try {
-            maskedMessage = mask(message);
-        } catch (Exception e) {
-            maskedMessage = message;
+    public String convert(ILoggingEvent event) {
+        String message = event.getFormattedMessage();
+        String maskedMsg = message;
+        try{
+            maskedMsg = mask(message);
+        }catch (Exception ex){
+            maskedMsg = message;
         }
-        outputMessage.append(maskedMessage);
-
+        return maskedMsg;
     }
-    private String mask(String message) throws Exception {
+
+
+    private String mask(String message) {
         Matcher matcher =null;
         StringBuffer buffer = new StringBuffer();
-        StringBuffer tempBuffer = new StringBuffer();
-        String urls = null;
-        Matcher tempMatcher =null;
-        boolean showEncrypted = Boolean.parseBoolean(encryptionProperty);
         matcher = DTO_PATTERN.matcher(message);
-        maskMatcher(matcher, buffer,DTO_REPLACEMENT_REGEX,showEncrypted);
+        maskMatcher(matcher, buffer,DTO_REPLACEMENT_REGEX);
         message=buffer.toString();
         buffer.setLength(0);
 
         matcher = JSON_PATTERN.matcher(message);
-        maskMatcher(matcher, buffer,JSON_REPLACEMENT_REGEX,showEncrypted);
+        maskMatcher(matcher, buffer,JSON_REPLACEMENT_REGEX);
         message=buffer.toString();
         buffer.setLength(0);
 
         matcher = PHONE_DTO_PATTERN.matcher(message);
-        maskMatcher(matcher, buffer,PHONE_DTO_REPLACEMENT_REGEX,showEncrypted);
+        maskMatcher(matcher, buffer,PHONE_DTO_REPLACEMENT_REGEX);
         message=buffer.toString();
         buffer.setLength(0);
 
         matcher = PHONE_JSON_PATTERN.matcher(message);
-        maskMatcher(matcher, buffer,PHONE_JSON_REPLACEMENT_REGEX,showEncrypted);
+        maskMatcher(matcher, buffer,PHONE_JSON_REPLACEMENT_REGEX);
         message=buffer.toString();
         buffer.setLength(0);
 
         matcher = EMAIL_DTO_PATTERN.matcher(message);
-        maskMatcher(matcher, buffer,EMAIL_DTO_REPLACEMENT_REGEX,showEncrypted);
+        maskMatcher(matcher, buffer,EMAIL_DTO_REPLACEMENT_REGEX);
         message=buffer.toString();
         buffer.setLength(0);
 
         matcher = EMAIL_JSON_PATTERN.matcher(message);
-        maskMatcher(matcher, buffer,EMAIL_JSON_REPLACEMENT_REGEX,showEncrypted);
+        maskMatcher(matcher, buffer,EMAIL_JSON_REPLACEMENT_REGEX);
         message = buffer.toString();
         buffer.setLength(0);
 
-
         return message;
     }
-    private StringBuffer maskMatcher(Matcher matcher, StringBuffer buffer, String maskStr, boolean showEncrypted) throws Exception {
+    private StringBuffer maskMatcher(Matcher matcher, StringBuffer buffer, String maskStr) {
         while (matcher.find()) {
             matcher.appendReplacement(buffer,maskStr);
         }
